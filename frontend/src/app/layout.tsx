3c9ce945cb644ec20cab670baa5de6da9ae5789b
sumit-maurya18 <techaid185@gmail.com>
2026-04-03 22:41:59 +0530

    fixed dark mode issue

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -31,8 +31,8 @@ export default function RootLayout({
       <body>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
-          enableSystem
+          defaultTheme="light"
+          enableSystem = {false}
           disableTransitionOnChange
         >
           <AppShell>
